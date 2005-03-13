3103109ffef7764d7f122260a4ad5160193ae1e3
mik3_jon3s <mik3_jon3s@9b953224-67f1-0310-8b4b-ab478a3e8703>
2005-03-13 13:02:03 +0000

    Deleted my stupid hivemind test files

--- coriolis/trunk/src/test/org/HivemindTest.java
+++ coriolis/trunk/src/test/org/HivemindTest.java
@@ -14,18 +14,6 @@ public class HivemindTest extends TestCase {
     
     public void testThis() {
         Registry registry = RegistryBuilder.constructDefaultRegistry();
-//
-//        
-//        A a = (A)registry.getService(A.class);
-//        assertTrue(a.aye().equals("a"));
-//        
-//        B b = (B) registry.getService(B.class);
-//        assertTrue(b.bee().equals("b"));
-//        
-//        C c = (C) registry.getService(C.class);
-//        assertTrue(c.getAaa().equals("a"));
-//        assertTrue(c.getBbb().equals("b"));
-//        
   
         ISessionManager sessionManager = (ISessionManager) registry.getService(ISessionManager.class);
         sessionManager.beginTransaction();
