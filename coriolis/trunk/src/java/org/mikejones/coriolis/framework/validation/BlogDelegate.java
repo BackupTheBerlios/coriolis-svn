333f318fbc1c74f43ed49f9bed764b64e1ed77e9
mik3_jon3s <mik3_jon3s@9b953224-67f1-0310-8b4b-ab478a3e8703>
2005-03-23 09:25:24 +0000

    tried to write the delegate to handle the valid field but the whole things a bit poo

--- coriolis/trunk/src/java/org/mikejones/coriolis/framework/validation/BlogDelegate.java
+++ coriolis/trunk/src/java/org/mikejones/coriolis/framework/validation/BlogDelegate.java
@@ -3,10 +3,86 @@
  */
 package org.mikejones.coriolis.framework.validation;
 
+import org.apache.tapestry.IMarkupWriter;
+import org.apache.tapestry.IRequestCycle;
+import org.apache.tapestry.form.IFormComponent;
+import org.apache.tapestry.valid.IValidator;
+import org.apache.tapestry.valid.ValidationDelegate;
+
 /**
  *
  * @author <a href="mailTo:michael.jones@anite.com">Mike</a>
  */
-public class BlogDelegate {
+public class BlogDelegate extends ValidationDelegate {
+    
+    /**
+     * Called BEFORE the FieldLabel renders
+     * @param writer
+     * @param cycle
+     * @param component
+     * @param validator
+     */
+    public void writeLabelPrefix(IMarkupWriter writer, IRequestCycle cycle,
+            IFormComponent component, IValidator validator) {
+        
+        writer.begin("label");
+        
+        if(isInError(component)) {
+            writer.attribute("class", "error");
+        }
 
+    }
+    
+    /**
+     * Called AFTER the FieldLabel renders
+     * @param writer
+     * @param cycle
+     * @param component
+     */
+    public void writeLabelSuffix(IMarkupWriter writer, IRequestCycle cycle,
+            IFormComponent component) {
+        writer.end();
+        
+    }
+    
+    /**
+     * Called BEFORE ValidField renders
+     * 
+     * @param writer
+     * @param cycle
+     * @param component
+     */
+    public void writePrefix(IMarkupWriter writer, IRequestCycle cycle,
+            IFormComponent component) {
+        
+    }
+    
+    /**
+     * Called AFTER ValidField renders
+     * @param writer
+     * @param cycle
+     * @param component
+     */
+    public void writeSuffix(IMarkupWriter writer, IRequestCycle cycle,
+            IFormComponent component, IValidator validator) {
+        if(validator!=null && validator.isRequired()) {
+            writer.print("*");
+            
+        }
+    }
+    
+    /**
+     * Called AS the ValidField renders
+     * @param writer
+     * @param cycle
+     * @param component
+     */
+    public void writeAttributes(IMarkupWriter writer, IRequestCycle cycle,
+            IFormComponent component, IValidator validator) {  
+        
+        if( isInError(component)) {
+            writer.attribute("class", "error");            
+        }
+       
+    }
 }
