bf13519cbe879c95ee036e00b06821ce85e0f568
mik3_jon3s <mik3_jon3s@9b953224-67f1-0310-8b4b-ab478a3e8703>
2005-12-05 23:38:26 +0000

    modified interface

--- coriolis/trunk/src/main/java/org/mikejones/coriolis/managers/impl/HibernatePostManager.java
+++ coriolis/trunk/src/main/java/org/mikejones/coriolis/managers/impl/HibernatePostManager.java
@@ -5,11 +5,31 @@ package org.mikejones.coriolis.managers.impl;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.mikejones.coriolis.managers.api.PostManager;
 import org.mikejones.coriolis.om.Post;
 
 
 public class HibernatePostManager implements PostManager {
+    
+    public Session session;
+    
+    /**
+     * Get the injected session
+     * @return
+     */
+    public Session getSession() {
+        return session;
+    }
+
+    /**
+     * Method to inject the session
+     * @param session
+     */
+    public void setSession(Session session) {
+        this.session = session;
+    }
 
     public Post getPost(int index) {
         // TODO Auto-generated method stub
@@ -17,33 +37,34 @@ public class HibernatePostManager implements PostManager {
     }
 
     public Post getPost(Integer id) {
-        // TODO Auto-generated method stub
-        return null;
+        return (Post) session.load(Post.class, id);
     }
 
+    /**
+     * Return all the posts
+     */    
+    @SuppressWarnings("unchecked")
     public List<Post> getPosts() {
-        // TODO Auto-generated method stub
-        return null;
+        return session.createQuery("from " + Post.class.getName()).list();
     }
 
-    public void addPost(Post post) {
-        // TODO Auto-generated method stub
-        
+    public void savePost(Post post) {
+        Transaction t = session.beginTransaction();
+        session.save(post);
+        t.commit();
     }
 
-    public void removePost(Post post) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    public void removePost(Integer id) {
+    public void deletePost(Post post) {
         // TODO Auto-generated method stub
         
     }
 
-    public void saveOrUpdate(Post post) {
-        // TODO Auto-generated method stub
-        
+    public void deletePost(Integer id) {
+        Transaction t = session.beginTransaction();
+        Post post = getPost(id);
+        session.delete(post);
+        t.commit();        
     }
 
+    
 }
