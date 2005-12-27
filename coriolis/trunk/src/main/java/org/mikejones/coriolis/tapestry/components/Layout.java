1444f412f2f706b27facd9919962cbad92afbbbe
mik3_jon3s <mik3_jon3s@9b953224-67f1-0310-8b4b-ab478a3e8703>
2005-12-27 23:12:03 +0000

    update titles and stuff

--- coriolis/trunk/src/main/java/org/mikejones/coriolis/tapestry/components/Layout.java
+++ coriolis/trunk/src/main/java/org/mikejones/coriolis/tapestry/components/Layout.java
@@ -3,10 +3,16 @@
  */
 package org.mikejones.coriolis.tapestry.components;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.tapestry.BaseComponent;
+import org.apache.tapestry.IAsset;
 import org.apache.tapestry.IRequestCycle;
+import org.apache.tapestry.annotations.Asset;
 import org.apache.tapestry.annotations.InjectObject;
 import org.apache.tapestry.annotations.InjectState;
+import org.apache.tapestry.annotations.Parameter;
 import org.mikejones.coriolis.managers.api.BlogManager;
 import org.mikejones.coriolis.om.Blog;
 import org.mikejones.coriolis.tapestry.framework.aso.BlogVisit;
@@ -15,6 +21,9 @@ public abstract class Layout extends BaseComponent {
 	
 	public abstract Blog getBlog();
 	
+	@Parameter
+	public abstract String getWindowSubtitle();
+	
 	public abstract void setBlog(Blog blog);	
 
     @InjectState("blogVisit")
@@ -22,11 +31,43 @@ public abstract class Layout extends BaseComponent {
     
     @InjectObject("service:coriolis.managers.BlogManager")
     public abstract BlogManager getBlogManager();
+    
+    @Asset("s/base.css")
+    public abstract IAsset getBaseStyle();
+    
+    @Asset("s/green.css")
+    public abstract IAsset getGreenStyle();
+    
+    @Asset("s/calendar.css")
+    public abstract IAsset getCalendarStyle();
+    
+    private List<IAsset> styles;
+    
+    public List<IAsset> getStyleSheets() {
+    	return styles; 	
+    }
 
     public abstract void setIsLoggedIn(boolean isLoggedIn);
+    
+    public String getWindowTitle() {
+    	String subTitle = getWindowSubtitle();
+    	
+    	if(subTitle == null) {
+    		return getBlog().getTitle();
+    	}
+    	return getBlog().getTitle() +" : " + subTitle;
+    }
 
     protected void prepareForRender(IRequestCycle cycle) {
         setIsLoggedIn(getPage().getEngine().getInfrastructure().getApplicationStateManager().exists("blogVisit"));
+        
+        if (styles == null) {
+    		styles = new ArrayList<IAsset>();
+    		styles.add(getBaseStyle());
+    		styles.add(getGreenStyle());
+    		styles.add(getCalendarStyle());
+    		
+    	}
         
         if(getBlog()==null) {
         	Blog blog = new Blog();
